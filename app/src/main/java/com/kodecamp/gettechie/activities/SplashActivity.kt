a83032f6552b3438e4ce9a6f72655857ddd10eee
ajdimarvellousgo@gmail.com <ajdimarvellousgo@gmail.com>
2022-06-27 23:13:14 +0100

    set up dev

--- app/src/main/java/com/kodecamp/gettechie/activities/SplashActivity.kt
+++ app/src/main/java/com/kodecamp/gettechie/activities/SplashActivity.kt
@@ -3,7 +3,7 @@ package com.kodecamp.gettechie.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.kodecamp.gettechie.R
-
+//splass screen
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
