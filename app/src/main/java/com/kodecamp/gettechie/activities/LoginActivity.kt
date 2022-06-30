0b82d6daa912e1fd25a2037bdf5b5de933f79b4e
ajdimarvellousgo@gmail.com <ajdimarvellousgo@gmail.com>
2022-06-30 21:56:01 +0100

    Remember_me_and_Exit_dialog

--- app/src/main/java/com/kodecamp/gettechie/activities/LoginActivity.kt
+++ app/src/main/java/com/kodecamp/gettechie/activities/LoginActivity.kt
@@ -1,9 +1,14 @@
 package com.kodecamp.gettechie.activities
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.CheckBox
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
@@ -19,6 +24,18 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         val google_login: ImageView = findViewById(R.id.log_in_button)
+        val log_in:Button=findViewById(R.id.log_in_auth)
+        val check:CheckBox=findViewById(R.id.checkBox)
+        val sharedPref:SharedPreferences = getSharedPreferences("checkbox",MODE_PRIVATE)
+        val checkbox: String? = sharedPref.getString("remember_key","")
+        if(checkbox.equals("true")){
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+        else if (checkbox.equals("false")){
+            var toast = Toast.makeText(this,"Please Sign In", Toast.LENGTH_LONG)
+            toast.show()
+        }
         val gso= GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestEmail()
             .build()
@@ -29,6 +46,24 @@ class LoginActivity : AppCompatActivity() {
             startActivityForResult(signInIntent, RC_SIGN_IN);
 
         }
+        log_in.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+        check.setOnCheckedChangeListener { compoundButton, b ->
+            if(compoundButton.isChecked){
+                val sharedPref:SharedPreferences = getSharedPreferences("checkbox",MODE_PRIVATE)
+                val editor:SharedPreferences.Editor=sharedPref.edit()
+                editor.putString("remember_key","true")
+                editor.apply()
+            }
+            else if (!compoundButton.isChecked){
+                val sharedPref:SharedPreferences = getSharedPreferences("checkbox",MODE_PRIVATE)
+                val editor:SharedPreferences.Editor=sharedPref.edit()
+                editor.putString("remember_key","false")
+                editor.apply()
+            }
+        }
     }
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
@@ -38,18 +73,12 @@ class LoginActivity : AppCompatActivity() {
             handleSignInResult(task)
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
-            Log.v("bloob","i'm working")
+//            Log.v("bloob","i'm working")
         }
     }
     private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
         try {
             val account: GoogleSignInAccount = completedTask.getResult(ApiException::class.java)
-
-//            user_name.text=account.displayName
-            // Signed in successfully, show authenticated UI.
-//            val myIntent = Intent(this, IntroFRagment::class.java)
-//            startActivity(myIntent)
-//            finish()
          } catch (e: ApiException) {
         }
     }
