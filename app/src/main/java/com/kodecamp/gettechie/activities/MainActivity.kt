8d60392e22e1a06a7305a774b5878d4fbb1f66c8
ajdimarvellousgo@gmail.com <ajdimarvellousgo@gmail.com>
2022-07-01 16:08:35 +0100

    Remember_me_and_Exit_dialog

--- app/src/main/java/com/kodecamp/gettechie/activities/MainActivity.kt
+++ app/src/main/java/com/kodecamp/gettechie/activities/MainActivity.kt
@@ -1,22 +1,13 @@
 package com.kodecamp.gettechie.activities
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
 import android.widget.Toast
-import com.google.android.gms.auth.api.Auth
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInApi
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.common.api.PendingResult
-import com.google.android.gms.common.api.ResultCallback
-import com.google.android.gms.common.api.Status
-import com.google.android.gms.tasks.OnCompleteListener
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.kodecamp.gettechie.R
 import kotlin.system.exitProcess
+
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,7 +32,7 @@ class MainActivity : AppCompatActivity() {
     var backPressedTime: Long = 0
     override fun onBackPressed() {
         if (backPressedTime + 3000 > System.currentTimeMillis()) {
-            showdialog()
+            show_dialog()
         } else {
 
             Toast.makeText(this, "Press back again to leave the app.", Toast.LENGTH_LONG).show()
@@ -49,15 +40,18 @@ class MainActivity : AppCompatActivity() {
         backPressedTime = System.currentTimeMillis()
 
     }
-
-    private fun showdialog() {
-        MaterialAlertDialogBuilder(this)
+    private fun show_dialog() {
+        MaterialAlertDialogBuilder(this, R.style.AlertDialogTheme)
             .setTitle("Get Techie")
-            .setMessage("Do you want to close this application")
-            .setCancelable(true)
-            .setNegativeButton("YES"){_, _ ->
+            .setMessage("Do you want to close this application.")
+            .setPositiveButton(
+                "YES"
+            ) { dialogInterface, i ->
                 finish()
             }
+            .setNeutralButton(
+                "CANCEL"
+            ) { dialogInterface, i -> }
             .show()
     }
     override fun onDestroy() {
