8b023c2295b2641e7e67fe3251ffed6ea69ddba8
PreciousAnyi <jkpresh@gmail.com>
2022-07-01 21:37:30 -0700

    designed onboarding screens

--- app/src/main/java/com/kodecamp/gettechie/SecondOnboardingScreen.kt
+++ app/src/main/java/com/kodecamp/gettechie/SecondOnboardingScreen.kt
@@ -10,7 +10,7 @@ import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.card.MaterialCardView
 
 
-class FirstOnboardingScreen : Fragment() {
+class SecondOnboardingScreen : Fragment() {
 
 
     override fun onCreateView(
@@ -18,10 +18,10 @@ class FirstOnboardingScreen : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_first_onboarding_screen, container, false)
+        val view = inflater.inflate(R.layout.fragment_second_onboarding_screen, container, false)
 
         val viewpager = activity?.findViewById<ViewPager2>(R.id.viewpager2)
-        view.findViewById<MaterialCardView>(R.id.skip).setOnClickListener {
+        view.findViewById<MaterialCardView>(R.id.skip2).setOnClickListener {
 
             viewpager?.currentItem = 2
         }
