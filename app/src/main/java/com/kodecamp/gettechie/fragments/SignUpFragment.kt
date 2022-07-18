cc9a5ad7af3bf561fc1e9fab7b2f528be2b734c4
PreciousAnyi <jkpresh@gmail.com>
2022-07-18 03:36:38 -0700

    profile & edit profile layout design

--- app/src/main/java/com/kodecamp/gettechie/fragments/SignUpFragment.kt
+++ app/src/main/java/com/kodecamp/gettechie/fragments/SignUpFragment.kt
@@ -5,17 +5,17 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.util.Patterns
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.widget.doOnTextChanged
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.fragment.app.viewModels
-import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
@@ -243,22 +243,22 @@ class SignUpFragment : Fragment() {
         lifecycleScope.launch {
             launch {
                 viewModel.email.collect {
-                    binding.EmailEdit.setText(viewModel.email.toString())
+                    binding.EmailEdit.setText(it)
                 }
             }
             launch {
                 viewModel.password.collect {
-                    binding.PasswordEdit.setText(viewModel.password.toString())
+                    binding.PasswordEdit.setText(it)
                 }
             }
             launch {
                 viewModel.confirmPassword.collect {
-                    binding.ConfirmPasswordEdit.setText(viewModel.confirmPassword.toString())
+                    binding.ConfirmPasswordEdit.setText(it)
                 }
             }
             launch {
                 viewModel.name.collect {
-                    binding.NameEdit.setText(viewModel.name.toString())
+                    binding.NameEdit.setText(it)
                 }
             }
             super.onResume()
