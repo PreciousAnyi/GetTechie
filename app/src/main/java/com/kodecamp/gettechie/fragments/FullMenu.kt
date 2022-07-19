00f0d435a3dffd66846a2eb6ac962c57e70bfed4
Ojerry <onotu128@gmail.com>
2022-07-19 11:34:47 +0100

    implemnted full screen menu and fixed facebook authentication

--- app/src/main/java/com/kodecamp/gettechie/fragments/FullMenu.kt
+++ app/src/main/java/com/kodecamp/gettechie/fragments/FullMenu.kt
@@ -5,18 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.kodecamp.gettechie.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [FullMenu.newInstance] factory method to
- * create an instance of this fragment.
- */
 class FullMenu : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
@@ -25,8 +20,6 @@ class FullMenu : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
         }
     }
 
@@ -37,24 +30,13 @@ class FullMenu : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_full_menu, container, false)
     }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FullMenu.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            FullMenu().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val menuBackBtn : ImageView? = activity?.findViewById(R.id.fullMenuBackBtn)
+        menuBackBtn?.setOnClickListener{
+            findNavController().navigate(R.id.action_fullMenuFragment_to_bottomMenuFragment)
+        }
     }
+
+
+
 }
